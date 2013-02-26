acb6f63c1e9ca382e18ebb5a9cb2a66e6d452d1b
Dirk Colbry <colbrydi@msu.edu>
2013-02-26 06:44:52 -0800

    AverageBar that uses PIL

--- AverageBar.py
+++ AverageBar.py
@@ -8,17 +8,45 @@
 # <codecell>
 
 import os
+import Image as img 
 import fnmatch
-import skimage.color as color
 import numpy as np
+import subprocess
 
+def video2img(infile='/Volumes/Documents/colbrydi/Documents/DirksWork/2013 iCER VIdeos/GlobusOnline/media/GlobusOnline.mp4'):
+    path, ext = os.path.splitext(infile)
+    dir, filename = os.path.split(path)
+    subprocess.call(['mkdir', '-p', filename])
+    subprocess.check_output(['ffmpeg', '-i', infile, '-r', '1', '-f', 'image2', './'+filename+'/image-%6d.png'])
+    return filename
+
+def writeim(im,filename):
+    im = im.reshape(im.shape[0]*im.shape[1],im.shape[2])
+    if len(im[0]) == 3:
+        im = numpy.c_[im, 255*numpy.ones((len(im),1), numpy.uint8)]
+    im2 = Image.frombuffer('RGBA', size, arr.tostring(), 'raw', mode, 0,1)
+    im2.save(filename)
+
+def readim(filename):
+    im = img.open(filename) 
+    print(im)
+    im2 = np.asarray(im.getdata())
+    print(im2)
+    print(im2.shape)
+    #im = img.imread(filename)
+    return im2;
+    
 def AverageBar(indir='/Volumes/Documents/colbrydi/Documents/DirksWork/chamview/ChamB/'):
     tot = 0.0;
+    R = np.array([0,0,0]);
+    G = np.array([0,0,0]);
+    B = np.array([0,0,0]);
     for root, dirs, filenames in os.walk(indir):
         for f in filenames:
             if fnmatch.fnmatch(f,'*.png'):
-                im = img.imread(os.path.join(root,f))
+                im = readim(os.path.join(root,f))
                 sz = im.shape[0]
+                #print(sz)
                 r = np.zeros((sz,1))
                 g = np.zeros((sz,1))
                 b = np.zeros((sz,1))
@@ -34,39 +62,26 @@ def AverageBar(indir='/Volumes/Documents/colbrydi/Documents/DirksWork/chamview/C
                     G = np.append(G, g, axis=1)
                     B = np.append(B, b, axis=1)
                 tot=tot+1
+    if tot==0:
+        print('ERROR - No files found in '+indir)
+        return '' 
     im3 = np.zeros((R.shape[0],R.shape[1], 3))
     im3[:,:,0] = R
     im3[:,:,1] = G
     im3[:,:,2] = B 
     return im3
 
-# <codecell>
-
 import time
 tic = time.time()
-im4 = AverageBar()
+im4 = AverageBar('video1')
 toc = time.time()
 print(toc-tic)
 
-# <codecell>
+#x = '/Volumes/Documents/colbrydi/Documents/DirksWork/2013 iCER VIdeos/GlobusOnline/media/GlobusOnline.mp4'
+#im = AverageBar('./GlobusOnline/')
 
-plt.imshow(im4)
-
-# <codecell>
-
-x = '/Volumes/Documents/colbrydi/Documents/DirksWork/2013 iCER VIdeos/GlobusOnline/media/GlobusOnline.mp4'
-im = AverageBar('./GlobusOnline/')
-plt.imshow(im)
-
-# <codecell>
-
-x = '/Volumes/Documents/colbrydi/Documents/DirkArchive/2012/2012 Break Videos/HPCCUSB/media/HPCCUSB.mp4'
-im = AverageBar('./HPCCUSB/')
-plt.imshow(im)
-
-# <codecell>
+#x = '/Volumes/Documents/colbrydi/Documents/DirkArchive/2012/2012 Break Videos/HPCCUSB/media/HPCCUSB.mp4'
+#im = AverageBar('./HPCCUSB/')
 
 
 # <codecell>
-
-
