5398d833f9041da6f83ff1af04fcfc72e991c228
Dirk Colbry <colbrydi@msu.edu>
2013-02-28 09:10:22 -0800

    HueBar also working on Gordon. Needed to add a few modules to myPy virtualenv to get working. Including Cython and skimage

--- HueBar.py
+++ HueBar.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-# <nbformat>2</nbformat>
-
-# <markdowncell>
-
 # ## ImageBar Function
 # 
 # Usage: 
@@ -12,192 +7,90 @@
 # 
 # TODO: Fix this function to allow for saturation and value.
 
-# <codecell>
-
-import matplotlib.image as img
-import matplotlib.pyplot as plt
+from imIO  import *
+import Image as img
+import sys
 import skimage.color as color
 import numpy as np
+import os
+import fnmatch
 
-def ImageBar(filename='', sz=''):
+def HueBar(filename='', sz=''):
     
     if filename == '':
         print('File not found');
         return
-    if filename != '':
-        #print(filename)
-        im = img.imread(filename)
-    #plt.imshow(im)
-    
+    else: 
+        im = readim(filename)
+   
     #Total number of pixels
-    tot = float(size(im,0) * size(im,1))
- 
+    tot = np.float(np.size(im,0) * np.size(im,1))
     #Determin the hight of the new bar image
     if sz == '':
-        sz = size(im,0)
+        sz = np.size(im,0)
      
     # Convert to HSV color space
     im2 = color.rgb2hsv(im)
     h = im2[:,:,0]
     s = im2[:,:,1]
     v = im2[:,:,2]
-    #plt.imshow(v)
-    #plt.colorbar()
-       
+
     #Remove dark colors
-    flag = tuple(i < 0.2 for i in v)
+    flag = tuple(i < 0.02 for i in v)
     flag = np.asarray(flag)
-    ztot = sum(flag)
-    h = np.asarray(h)
     h2 = h[~flag]
-    z = float(ztot)/tot * sz
-    X = zeros([z,1])
-    Y = ones((sz,1))
+    flag = np.double(flag)
+    ztot = sum(sum(flag))
+    h = np.asarray(h)
+    z = np.float(ztot)/tot * sz
+    X = np.zeros([z,1])
+    Y = np.ones((sz,1))*0.5
     Y[0:z,:] = 0
     
-    #print(tot)
-    #print(ztot)
-    
-    #Calculate histogram on color (H) space
-    n, bins = np.histogram(h2, bins=100)
-    n = n/tot * sz
-    for i in range(0,size(bins)-1):
-        next = np.ones([round(n[i]),1])*bins[i]
-        X = np.append(X, next, axis=0)
+    if np.size(h2) > 0:
+        #Calculate histogram on color (H) space
+        n, bins = np.histogram(h2, bins=100)
+        n = n/tot * sz
+        for i in range(0,np.size(bins)-1):
+            next = np.ones([round(n[i]),1])*np.average(bins[i]+bins[i+1])
+            X = np.append(X, next, axis=0)
+                
+        if np.size(X,0) < sz:
+            X = np.append(X, np.zeros([sz - np.size(X,1),1]), axis=0)
+        if np.size(X,0) > sz:
+            X= X[0:sz,:]
         
-        
-    if size(X,0) < sz:
-        X = np.append(X, zeros([sz - size(X,1),1]), axis=0)
-    if size(X,0) > sz:
-        X = X[0:sz,:]
-        
-    im3 = zeros((size(X,0),1, 3))
+    im3 = np.zeros((np.size(X,0),1, 3))
     im3[:,:,0] = X
-    im3[:,:,1] = 1
+    im3[:,:,1] = 1 
     im3[:,:,2] = Y     
-
         
-    return im3;
-    
-
-# <codecell>
-
-def extrapolatebar(ib):
-    vsz = 800
-    hsz = 40
-    br = zeros([vsz,hsz,3])
-    for c in range((0,hsz)):
-       br[:,c,:] = ib
-    bar = color.hsv2rgb(br)
-    return bar
-
-# <codecell>
-
-ib = ImageBar('/Volumes/Documents/colbrydi/Documents/DirksWork/chamview/ChamB/img-000339.png')
-
-# <codecell>
-
-bar = extrapolatebar(ib)
-plt.imshow(bar)
-
-# <markdowncell>
+    return im3,sz
 
 # ## makeVideoBar from directory of images
 # 
 # TODO: Fix this function and move the HSV integration to the previous funciton to allow for saturation and value.
 
-# <codecell>
-
-import os
-import fnmatch
 
 def makeVideoBar(indir='/Volumes/Documents/colbrydi/Documents/DirksWork/chamview/ChamB/'):
-    sz = 800
-    br = zeros([sz,1,3])
+    sz=''
+    br=''
     for root, dirs, filenames in os.walk(indir):
+        filenames.sort()
         for f in filenames:
-            if fnmatch.fnmatch(f,'*.png'):
-                ib = ImageBar(os.path.join(root,f), sz);
-                br = np.append(br, ib, axis=1)
+            if fnmatch.fnmatch(f,'0*.jpeg'):
+                (ib, sz) = HueBar(os.path.join(root,f),sz);
+                if br == '':
+                    br = ib
+                else:
+                    br = np.append(br, ib, axis=1)
     bar = color.hsv2rgb(br)
+    bar = bar * 255
     return bar
-
-# <codecell>
 
 import time
 tic = time.time()
-im4 = makeVideoBar()
+im4 = makeVideoBar(sys.argv[1])
 toc = time.time()
 print(toc-tic)
-
-# <codecell>
-
-plt.imshow(im4)
-
-# <codecell>
-
-import subprocess
-def video2img(infile='/Volumes/Documents/colbrydi/Documents/DirksWork/2013 iCER VIdeos/GlobusOnline/media/GlobusOnline.mp4'):
-    path, ext = os.path.splitext(x)
-    dir, filename = os.path.split(path)
-    subprocess.call(['mkdir', '-p', filename])
-    subprocess.check_output(['ffmpeg', '-i', infile, '-r', '1', '-f', 'image2', './'+filename+'/image-%6d.png'])
-    return filename
-
-# <codecell>
-
-import subprocess
-def makeBarFromVideo(infile='/Volumes/Documents/colbrydi/Documents/DirksWork/2013 iCER VIdeos/GlobusOnline/media/GlobusOnline.mp4'):
-    folder=video2img(infile)
-    tic = time.time()
-    im4 = makeVideoBar(folder)
-    toc = time.time()
-    print(toc-tic)
-    plt.imshow(im4)
-    return im4
-    
-
-# <codecell>
-
-x = '/Volumes/Documents/colbrydi/Documents/DirksWork/2013 iCER VIdeos/GlobusOnline/media/GlobusOnline.mp4'
-im = makeBarFromVideo(x)
-
-# <codecell>
-
-x = '/Volumes/Documents/colbrydi/Documents/DirkArchive/2012/2012 Break Videos/HPCCUSB/media/HPCCUSB.mp4'
-im = makeBarFromVideo(x)
-
-# <codecell>
-
-
-# <codecell>
-
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-
-# <codecell>
-
-print(x)
-
-# <codecell>
-
-f = tuple( i < 5 for i in x)
-
-# <codecell>
-
-print(f)
-
-# <codecell>
-
-true + false
-
-# <codecell>
-
-True + False
-
-# <codecell>
-
-True + True
-
-# <codecell>
-
-
+writeim(im4, sys.argv[1]+'/HueBar.jpeg')
